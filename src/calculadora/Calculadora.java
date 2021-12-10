31eb8dae47bee56d879a2f568773190855c940b3
= <=>
2021-12-10 20:34:27 +0100

    He programado la suma y la resta

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,5 +1,15 @@
 package calculadora;
 
 public class Calculadora {
-
+	public Calculadora() {
+		
+	}
+	
+	public void sumar(int n1, int n2) {
+		System.out.println("Suma = " + (n1 + n2));
+	}
+	
+	public void restar(int n1, int n2) {
+		System.out.println("Resta = " + (n1 - n2));
+	}
 }
--- src/calculadora/Main.java
+++ src/calculadora/Main.java
@@ -1,5 +1,8 @@
 package calculadora;
 
 public class Main {
-
+	public static void main(String[] args) {
+		Calculadora c = new Calculadora();
+		c.sumar(2, 3);
+	}
 }
